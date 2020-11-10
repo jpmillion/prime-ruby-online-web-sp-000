f7e52ac82422fcbcc1b780beb03c5a7a83abcc97
John Million <jpmillion@comcast.net>
2020-11-10 23:02:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(int)
-  if int   && (2..int).find { |i| int / i == 0 }
+  if int  == 2 || (2..int).find { |i| int / i == 0 } == nil
     return true  
   else
     return false
